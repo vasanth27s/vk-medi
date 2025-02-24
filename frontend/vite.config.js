a6b8a4f1a08646fb4f4138960a946d541ab3fd6c
vasanth <vasanth2003s@gmail.com>
2025-02-24 16:27:30 +0530

    vk-medi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
-  },
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
   server: {
-    port: 3000,
+    historyApiFallback: true,
   },
-});
+})

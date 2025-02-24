372bde9439db7e5dfbaa318e62959e05b6516e1f
vasanth <vasanth2003s@gmail.com>
2025-02-24 14:58:25 +0530

    vk-mediapp

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,3 @@
-export const API_URL =  process.env.NODE_ENV === "production" ? "" : "http://localhost:8080"
-
-   
+export const API_URL = process.env.NODE_ENV === "production" 
+  ? "https://vk-mediapp.onrender.com" 
+  : "http://localhost:8080";
